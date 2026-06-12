72fa41e995ea4bcb926f28e65a86ed611bc0aac8
samu7173 <samuelmatas1997@gmail.com>
2026-06-12 21:11:26 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/project/books_api/BooksApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.books_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApiApplication.class, args);
+	}
+
+}
