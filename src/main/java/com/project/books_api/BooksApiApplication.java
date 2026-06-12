8a599a7b66377c185e5cead549764bae61023b11
samu7173 <samuelmatas1997@gmail.com>
2026-06-13 01:42:32 +0200

    Implement Gutendex client integration

--- src/main/java/com/project/books_api/BooksApiApplication.java
+++ src/main/java/com/project/books_api/BooksApiApplication.java
@@ -1,15 +1,16 @@
 package com.project.books_api;
 
+import com.project.books_api.infrastructure.gutendex.GutendexClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class BooksApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BooksApiApplication.class, args);
 	}
-
 }
--- src/main/java/com/project/books_api/infrastructure/gutendex/GutendexClient.java
+++ src/main/java/com/project/books_api/infrastructure/gutendex/GutendexClient.java
@@ -1,15 +1,45 @@
 package com.project.books_api.infrastructure.gutendex;
 
 import com.project.books_api.config.GutendexProperties;
+import com.project.books_api.infrastructure.gutendex.dto.GutendexBookDto;
+import com.project.books_api.infrastructure.gutendex.dto.GutendexSearchResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
 @Component
-@RequiredArgsConstructor
 public class GutendexClient {
 
     private final RestClient restClient;
     private final GutendexProperties properties;
 
+    public GutendexClient(
+            RestClient restClient,
+            GutendexProperties properties
+    ) {
+        this.restClient = restClient;
+        this.properties = properties;
+    }
+
+    public GutendexBookDto getBook(Long id) {
+
+        return restClient.get()
+                .uri(properties.baseUrl() + "/books/" + id + "/")
+                .retrieve()
+                .body(GutendexBookDto.class);
+    }
+
+    public GutendexSearchResponseDto searchBooks(String query) {
+
+        var entity = RestClient.create()
+                .get()
+                .uri(properties.baseUrl() + "/books/?search=" + query)
+                .retrieve()
+                .toEntity(String.class);
+
+        System.out.println("STATUS: " + entity.getStatusCode());
+        System.out.println("BODY: " + entity.getBody());
+
+        return null;
+    }
 }
